7ea6011ae558e4fb4303832a0647f703fa824fb8
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-07-09 14:42:54 +0300

    association table

--- server/models.py
+++ server/models.py
@@ -1,23 +1,29 @@
 from datetime import datetime
-from flask import current_app
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
 
 db = SQLAlchemy()
 
+# Association table to store favorites relationship between users and recipes
+favorites = db.Table(
+    'favorites',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True),
+    db.Column('date_favorited', db.DateTime, default=datetime.utcnow)
+)
+
 class User(db.Model, SerializerMixin):
     __tablename__ = 'user'
-    serialize_rules = ('-password_hash', '-recipes', '-favorites', '-comments')
-
+    serialize_rules = ('-password_hash', '-favorites', '-recipes', '-comments')
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    recipes = db.relationship('Recipe', backref='author', lazy=True)
-    favorites = db.relationship('Favorite', backref='user', lazy=True)
-    comments = db.relationship('Comment', backref='user', lazy=True)
+
+    favorite_recipes = association_proxy('favorite_relationships', 'recipe')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -25,28 +31,22 @@ class User(db.Model, SerializerMixin):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipe'
-    serialize_rules = ('-user', '-comments')
+    serialize_rules = ('-users', '-comments')
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     ingredients = db.Column(db.Text, nullable=False)
     instructions = db.Column(db.Text, nullable=False)
+
+    users = db.relationship('User', secondary=favorites, backref=db.backref('favorite_relationships', lazy='dynamic'))
+
     comments = db.relationship('Comment', backref='recipe', lazy=True)
 
-
-class Favorite(db.Model, SerializerMixin):
-    __tablename__ = 'favorite'
-    serialize_rules = ('-user', '-recipe')
-    
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
-    
 
 class Comment(db.Model, SerializerMixin):
     __tablename__ = 'comment'
@@ -57,4 +57,5 @@ class Comment(db.Model, SerializerMixin):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+
     
