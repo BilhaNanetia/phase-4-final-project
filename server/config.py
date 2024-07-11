85a2789f55475815ef7153aa511b5b3201adc64b
Bilha <bilha.leposo@student.moringaschool.com>
2024-07-11 08:34:01 +0300

    updates

--- server/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your_jwt_secret_key'
-    SPOONACULAR_API_KEY = os.environ.get('SPOONACULAR_API_KEY') or 'your_spoonacular_api_key'
